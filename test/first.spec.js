82a9b315b4e83b0533260d28ec574d1967eeb5b8
Christo <christo@cryptodrunks.net>
2017-07-29 11:13:34 +0200

    build(lint): Use prettier and automatic linter fixing.

--- test/first.spec.js
+++ test/first.spec.js
@@ -1,9 +1,9 @@
-import {isEqual} from 'lodash/fp';
-import {assertForall, nat} from 'jsverify';
+import {isEqual} from "lodash/fp";
+import {assertForall, nat} from "jsverify";
 
-import {add} from '../lib';
+import {add} from "../lib";
 
-describe('basic math', () => {
-  it('adds two numbers', () =>
+describe("basic math", () => {
+  it("adds two numbers", () =>
     assertForall(nat, nat, (a, b) => isEqual(add(a, b), b + a)));
 });
