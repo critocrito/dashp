69892fc5528f67ad95457fb6fb0a0f5fbb54a39a
Christo <christo@cryptodrunks.net>
2017-07-31 22:19:02 +0200

    refactor(future-spec): Use the anyArb to generate the tests.

--- test/future.spec.js
+++ test/future.spec.js
@@ -1,37 +1,16 @@
 import {isEqual} from "lodash/fp";
 import Promise from "bluebird";
-import {
-  assertForall,
-  oneof,
-  array,
-  number,
-  char,
-  string,
-  json,
-  bool,
-} from "jsverify";
+import {assertForall} from "jsverify";
 
+import {anyArb} from "./arbitraries";
 import future from "../lib/combinators/future";
 
 describe("Lifting values into a promise", () => {
-  const arb = oneof([
-    number,
-    char,
-    string,
-    json,
-    bool,
-    array(number),
-    array(char),
-    array(string),
-    array(json),
-    array(bool),
-  ]);
-
-  it("lifts any value into a promise", () =>
-    assertForall(arb, x =>
+  it("is equivalent to Promise.resolve", () =>
+    assertForall(anyArb, x =>
       Promise.all([future(x), Promise.resolve(x)]).spread(isEqual)
     ));
 
-  it("lpifts any value into a promise", () =>
-    assertForall(arb, x => future(x).then(isEqual(x))));
+  it("lifts any value into a promise", () =>
+    assertForall(anyArb, x => future(x).then(isEqual(x))));
 });
