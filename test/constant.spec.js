0a5145023a4c1e0f3ec3be1dbcc119a2b0584fd3
Christo <christo@cryptodrunks.net>
2017-07-31 23:34:48 +0200

    test(constant): Test promisified and non promisified arguments.

--- test/constant.spec.js
+++ test/constant.spec.js
@@ -1,10 +1,13 @@
 import {isEqual} from "lodash/fp";
 import {assertForall} from "jsverify";
 
-import {anyArb} from "./arbitraries";
+import {anyArb, maybePromisify} from "./arbitraries";
 import constant from "../lib/combinators/constant";
 
 describe("The constant operator", () => {
   it("always returns the same value", () =>
     assertForall(anyArb, x => constant(x)().then(isEqual(x))));
+
+  it("accepts non promisified and promisified arguments", () =>
+    assertForall(anyArb, x => constant(maybePromisify(x))().then(isEqual(x))));
 });
