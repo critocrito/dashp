f2f4e5fdee14275d2d42771a9c895a0ae9121a1a
Christo <christo@cryptodrunks.net>
2017-07-31 23:47:48 +0200

    test(compose): Use the type DSL and property of jsverify.

--- test/compose.spec.js
+++ test/compose.spec.js
@@ -1,24 +1,27 @@
 import {sum, isEqual} from "lodash/fp";
-import {assertForall, nat} from "jsverify";
+import {property} from "jsverify";
 
 import {maybePromisify, addP, addMaybeP} from "./arbitraries";
 import compose from "../lib/combinators/compose";
 
 describe("The compose combinator", () => {
-  it("accepts non promisified and promisified arguments", () =>
-    assertForall(nat, nat, nat, (x, y, z) =>
+  property(
+    "accepts non promisified and promisified arguments",
+    "nat",
+    "nat",
+    "nat",
+    (x, y, z) =>
       compose(
         maybePromisify(addMaybeP(x)),
         maybePromisify(addMaybeP(y)),
         maybePromisify(z)
       ).then(isEqual(sum([x, y, z])))
-    ));
+  );
 
-  it("is always associative", () =>
-    assertForall(nat, nat, nat, nat, (w, x, y, z) =>
-      Promise.all([
-        compose(addP(w), compose(addP(x), addP(y)), z),
-        compose(compose(addP(w), addP(x)), addP(y), z),
-      ]).then(([a, b]) => isEqual(a, b))
-    ));
+  property("is always associative", "nat", "nat", "nat", (w, x, y, z) =>
+    Promise.all([
+      compose(addP(w), compose(addP(x), addP(y)), z),
+      compose(compose(addP(w), addP(x)), addP(y), z),
+    ]).then(([a, b]) => isEqual(a, b))
+  );
 });
