ab8b53062dc1f9a7fd560452c8254b3e2a2c8938
Christo <christo@cryptodrunks.net>
2017-08-01 00:34:34 +0200

    test(map): Verify that the await syntax works.

---
+++ test/await.spec.js
@@ -0,0 +1,11 @@
+import {isEqual} from "lodash/fp";
+import {property} from "jsverify";
+import {addP} from "./arbitraries";
+import map from "../lib/combinators/map";
+
+describe("Compatibility with await", () => {
+  property("map", "array nat", "nat", (xs, y) =>
+    // eslint-disable-next-line lodash-fp/no-extraneous-function-wrapping
+    map(addP(y), xs).then(async ys => isEqual(await map(addP(y), xs), ys))
+  );
+});
