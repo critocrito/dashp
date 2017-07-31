23e1a21159dd89911524b2e2b9f4c841703c08c4
Christo <christo@cryptodrunks.net>
2017-08-01 00:03:55 +0200

    test(fold): Use the type DSL and property of jsverify.

--- test/fold.spec.js
+++ test/fold.spec.js
@@ -1,30 +1,38 @@
 import {sum, isEqual} from "lodash/fp";
-import {assertForall, array, nat} from "jsverify";
+import {property} from "jsverify";
 import Promise from "bluebird";
 
 import {maybePromisify, add, addP, addMaybeP} from "./arbitraries";
 import fold from "../lib/combinators/fold";
 
 describe("The fold combinator", () => {
-  it("accepts non promisified and promisified arguments", () =>
-    assertForall(array(nat), nat, (xs, y) =>
+  property(
+    "accepts non promisified and promisified arguments",
+    "array nat",
+    "nat",
+    (xs, y) =>
       fold(
         maybePromisify(addMaybeP),
         maybePromisify(y),
         maybePromisify(xs)
       ).then(isEqual(sum(xs) + y))
-    ));
+  );
 
-  it("produces the same result as a synchronous reduce", () =>
-    assertForall(array(nat), nat, (xs, y) =>
-      fold(add, y, xs).then(isEqual(xs.reduce(add, y)))
-    ));
+  property(
+    "produces the same result as a synchronous reduce",
+    "array nat",
+    "nat",
+    (xs, y) => fold(add, y, xs).then(isEqual(xs.reduce(add, y)))
+  );
 
-  it("produces the same result as reduce of Bluebird", () =>
-    assertForall(array(nat), nat, (xs, y) =>
+  property(
+    "produces the same result as reduce of Bluebird",
+    "array nat",
+    "nat",
+    (xs, y) =>
       Promise.all([
         Promise.reduce(xs, addP, y),
         fold(addP, y, xs),
       ]).then(([a, b]) => isEqual(a, b))
-    ));
+  );
 });
