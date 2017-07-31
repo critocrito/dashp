b88a13cae8002286bdd5c336752b5821f9f6671f
Christo <christo@cryptodrunks.net>
2017-07-31 23:44:44 +0200

    test(flow): Use the type DSL and property of jsverify.

--- test/flow.spec.js
+++ test/flow.spec.js
@@ -1,30 +1,37 @@
 import {map, reduce, identity, sum, isEqual} from "lodash/fp";
-import {assertForall, array, nat} from "jsverify";
+import {property} from "jsverify";
 
 import {maybePromisify, addP, addMaybeP} from "./arbitraries";
 import flow from "../lib/combinators/flow";
 import compose from "../lib/combinators/compose";
 
 describe("The flow combinator", () => {
-  it("composes a list of functions", () =>
-    assertForall(array(nat), nat, (xs, y) => {
-      const fs = map(addP, xs);
-      return flow(fs, y).then(isEqual(sum(xs) + y));
-    }));
+  property("composes a list of functions", "array nat", "nat", (xs, y) => {
+    const fs = map(addP, xs);
+    return flow(fs, y).then(isEqual(sum(xs) + y));
+  });
 
-  it("it accepts non promisified and promisified arguments", () =>
-    assertForall(array(nat), nat, (xs, y) => {
+  property(
+    "it accepts non promisified and promisified arguments",
+    "array nat",
+    "nat",
+    (xs, y) => {
       const fs = map(addMaybeP, xs);
       return flow(maybePromisify(fs), maybePromisify(y)).then(
         isEqual(sum(xs) + y)
       );
-    }));
+    }
+  );
 
-  it("is equivalent to composing compositions of functions", () =>
-    assertForall(array(nat), nat, (xs, y) => {
+  property(
+    "is equivalent to composing compositions of functions",
+    "array nat",
+    "nat",
+    (xs, y) => {
       const fs = map(addP, xs);
       const lhs = reduce((memo, x) => compose(memo, addP(x)), identity, xs);
       const rhs = flow(fs);
       return Promise.all([lhs(y), rhs(y)]).then(([a, b]) => isEqual(a, b));
-    }));
+    }
+  );
 });
