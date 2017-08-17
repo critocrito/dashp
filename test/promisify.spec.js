8c6f4f2a59b4f3befa56950c011f812262f79ec5
Christo <christo@cryptodrunks.net>
2017-08-17 23:29:30 +0200

    refactor: Use the promisify combinator to accept promises and non promises as arguments.

---
+++ test/promisify.spec.js
@@ -0,0 +1,18 @@
+import {property} from "jsverify";
+import {identity, isEqual} from "lodash/fp";
+import sinon from "sinon";
+import promisify from "../lib/promisify";
+import {isPromise} from "./arbitraries";
+
+describe("The promisification of arguments", () => {
+  property("returns a promise", "nat", x => isPromise(promisify(identity)(x)));
+
+  property("promisifies its arguments", "nat", x =>
+    promisify(identity)(x).then(isEqual(x))
+  );
+
+  property("promisifies variadic functions", "array nat", xs => {
+    const spy = sinon.spy();
+    return promisify(spy)(...xs).then(() => spy.args[0].length === xs.length);
+  });
+});
