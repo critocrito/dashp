7b3e84bf3ed3ed2e89d743d54c34843af03794bd
Christo <christo@cryptodrunks.net>
2017-08-23 14:48:50 +0200

    build: Use rollup to create different packages.

---
+++ rollup.config.js
@@ -0,0 +1,45 @@
+import babel from "rollup-plugin-babel";
+import uglify from 'rollup-plugin-uglify';
+import pkg from "./package.json";
+
+const plugins = [
+  babel({
+    presets: [["es2015", {modules: false}]],
+    plugins: ["external-helpers"],
+    babelrc: false,
+    exclude: "node_modules/**",
+  }),
+];
+
+const minify = plugins.concat([uglify()]);
+
+export default [
+  {
+    input: "lib/index.js",
+    output: {
+      file: pkg.browser,
+      format: "umd",
+      sourcemap: true,  
+    },
+    name: "cobinatorsP",
+    plugins,
+  },
+  {
+    input: "lib/index.js",
+    output: {
+      file: `${pkg.browser.split('.')[0]}.min.js`,
+      format: "umd",
+      sourcemap: true,
+    },
+    name: "cobinatorsP",
+    minify,
+  },
+  {
+    input: "lib/index.js",
+    output: {
+      file: pkg.main,
+      format: "cjs",
+    },
+    plugins,
+  },
+];
