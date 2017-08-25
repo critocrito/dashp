f14b55acecede603a4a48bc3c34f44bbdfdfce2c
Christo <christo@cryptodrunks.net>
2017-08-25 13:15:06 +0200

    refactor(arbitraries): Import jsverify as object.

--- test/arbitraries.js
+++ test/arbitraries.js
@@ -1,10 +1,10 @@
 import {curry, isEqual} from "lodash/fp";
-import {oneof, array, number, char, string, json, bool, random} from "jsverify";
+import jsc from "jsverify";
 
 export const isEqualAry = xs => isEqual(...xs);
 
 export const maybePromisify = val => {
-  switch (random(0, 1)) {
+  switch (jsc.random(0, 1)) {
     case 0:
       return val;
     default:
@@ -16,17 +16,17 @@ export const add = curry((x, y) => x + y);
 export const addP = curry((x, y) => Promise.resolve(add(x, y)));
 export const addMaybeP = curry((x, y) => maybePromisify(add(x, y)));
 
-export const anyArb = oneof([
-  number,
-  char,
-  string,
-  json,
-  bool,
-  array(number),
-  array(char),
-  array(string),
-  array(json),
-  array(bool),
+export const anyArb = jsc.oneof([
+  jsc.number,
+  jsc.char,
+  jsc.string,
+  jsc.json,
+  jsc.bool,
+  jsc.array(jsc.number),
+  jsc.array(jsc.char),
+  jsc.array(jsc.string),
+  jsc.array(jsc.json),
+  jsc.array(jsc.bool),
 ]);
 
 export default {
