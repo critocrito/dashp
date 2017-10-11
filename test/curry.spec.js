7a04c597c50f04c84c8897800cd9799f73c48b0d
Christo <christo@cryptodrunks.net>
2017-10-11 15:17:27 +0200

    feat: Set the name attribute for every function.

--- test/curry.spec.js
+++ test/curry.spec.js
@@ -11,7 +11,7 @@ describe("The currying of functions", () => {
     (x, y) => {
       const f = (a, b) => a + b;
       const f1 = loCurry(f);
-      const f2 = curry2(f);
+      const f2 = curry2("f", f);
       return every(isEqual(f(x, y)), [f1(x, y), f2(x, y), f2(x)(y)]);
     }
   );
@@ -24,7 +24,7 @@ describe("The currying of functions", () => {
     (x, y, z) => {
       const f = (a, b, c) => a + b + c;
       const f1 = loCurry(f);
-      const f2 = curry3(f);
+      const f2 = curry3("f", f);
       return every(isEqual(f(x, y, z)), [
         f1(x, y, z),
         f2(x, y, z),
@@ -44,7 +44,7 @@ describe("The currying of functions", () => {
     (w, x, y, z) => {
       const f = (a, b, c, d) => a + b + c + d;
       const f1 = loCurry(f);
-      const f2 = curry4(f);
+      const f2 = curry4("f", f);
       return every(isEqual(f(w, x, y, z)), [
         f1(w, x, y, z),
         f2(w, x, y, z),
@@ -58,7 +58,12 @@ describe("The currying of functions", () => {
   );
 
   property("curry/variadic function pattern", "nat", "nat", "nat", (...xs) => {
-    const f = curry3((...args) => args.reduce((memo, a) => memo + a, 0));
+    const f = curry3("f", (...args) => args.reduce((memo, a) => memo + a, 0));
     return isEqual(xs.reduce((memo, a) => memo + a, 0), f(...xs));
+  });
+
+  property("set the function name", "string", s => {
+    const f = curry2(s, x => x);
+    return isEqual(f.name, s);
   });
 });
