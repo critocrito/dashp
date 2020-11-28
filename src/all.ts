0ffa74546e19dc82ac18bece39e107b2778836a0
Christo <christo@cryptodrunks.net>
2020-11-28 15:12:34 +0100

    fix: clean up types for all combinator

--- src/all.ts
+++ src/all.ts
@@ -1,6 +1,6 @@
 import {of} from "./future";
 import nameFn from "./internal/namefn";
 
-export default nameFn("all", (xs: Promise<unknown>[]) => (): Promise<unknown[]> =>
-  of(xs).then((ys) => Promise.all(ys.map(of))),
-);
+export default nameFn("all", <T extends unknown>(xs: T[]): (() => Promise<T[]>) => (): Promise<
+  T[]
+> => of(xs).then((ys) => Promise.all(ys.map(of))));
