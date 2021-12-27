1ac5a8a06433a996f4f11f8a7bac7242e70e21ce
Christo <christo@cryptodrunks.net>
2021-12-27 19:19:24 +0100

    refactor(curry): modify types

--- src/internal/curry.ts
+++ src/internal/curry.ts
@@ -97,11 +97,11 @@ export interface Curry10<T1, T2, T3, T4, T5, T6, T7, T8, T9, T10, R> {
   (t1: T1, t2: T2, t3: T3, t4: T4, t5: T5, t6: T6, t7: T7, t8: T8, t9: T9, t10: T10): R;
 }
 
-const ncurry = (n: number): DashFn<Tuple, unknown> => {
-  const localCurry = (
+const ncurry = (n: number): ((...args: any[]) => any) => {
+  const localCurry = <T extends DashFn<Tuple, unknown>>(
     name: string,
-    f: DashFn<Tuple, unknown>,
-    ...args: Tuple
+    f: T,
+    ...args: Parameters<T>
   ): DashFn<Tuple, unknown> => {
     // This allows checkTypes to throw nicer error messages.
     const localF = nameFn(`Future#${name}`, f);
